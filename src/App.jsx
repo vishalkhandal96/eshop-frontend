e04b320bb0e7e4a27059a2eec225322e5384c089
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-04-26 01:06:20 +0530

    delete navmenu.jsx file and images folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import { useState } from "react";
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import "./App.css";
-
-import Navbars from "./components/Navbars";
-import Navmenu from "./components/Navmenu";
-import ProductsPage from "./components/ProductPage";
-import ShopingCart from "./components/ShopingCart";
-import ProductDetails from "./components/ProductDetails";
-import ProductCard from "./components/ProductCard";
-import Checkout from "./components/Checkout";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  // const [theme, setTheme] = useState("light");
   return (
-    // <div className={`container ${theme}`}>
-    //   <Navbar theme={theme} setTheme={setTheme} />
-    // </div>
     <>
-      <Navbars />
+      <Navbar />
     </>
   );
 }
