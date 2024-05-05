dd72226407cf653fb95b8200120f4f2afd473759
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-05-05 19:23:55 +0530

    create brandlogo component

--- src/components/Header/BrandLogo.jsx
+++ src/components/Header/BrandLogo.jsx
@@ -1,15 +1,15 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 function BrandLogo() {
   return (
     <>
-      <div className="flex flex-shrink-0 items-center">
+      <Link className="flex flex-shrink-0 items-center" to={"/"}>
         <img
           className="h-8 w-auto hidden sm:block"
           src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg"
-          alt="Your Company"
+          alt="Eshop ecommerce"
         />
-      </div>
+      </Link>
     </>
   );
 }
