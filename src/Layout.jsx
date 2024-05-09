4754d21acd9f936d035474d33cdedd7fa3ca87bd
vishalkhandal96 <vishalkumarkhandal@gmail.com>
2024-05-09 19:38:59 +0530

    add Home router component in Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
+import Home from "./routes/Home";
 
 function Layout() {
   return (
     <div>
       <Header />
+      <Home />
       <main>
         <Outlet />
       </main>
